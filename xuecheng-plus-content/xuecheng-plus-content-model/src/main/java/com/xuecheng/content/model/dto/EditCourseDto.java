c481cb85a3369f023b4eeff3285edb8ed5fb9199
yuyang <15131696987@163.com>
2023-12-11 15:52:48 +0800

    修改课程

---
+++ xuecheng-plus-content/xuecheng-plus-content-model/src/main/java/com/xuecheng/content/model/dto/EditCourseDto.java
@@ -0,0 +1,13 @@
+package com.xuecheng.content.model.dto;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import io.swagger.annotations.ApiOperation;
+import lombok.Data;
+
+@Data
+@ApiModel(value = "EditCourseDto", description = "修改课程基本信息")
+public class EditCourseDto extends AddCourseDto{
+    @ApiModelProperty(value = "课程id", required = true)
+    private Long id;
+}
